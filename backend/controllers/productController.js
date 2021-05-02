e24704b0390bfc2f28bad082db1738aa3dba2f26
ShopITSupport <83328239+ShopITSupport@users.noreply.github.com>
2021-05-02 11:16:09 +0530

    Datahandling

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -20,3 +20,64 @@ exports.getProducts = async (req, res, next) => {
         products
     })
 }
+
+//Get single product details => /api/v1/product/:id
+exports.getSingleProduct = async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+
+    if(!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found'
+        })
+    }
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+}
+
+//Update Product => /api/v1/admin/product/:id
+
+exports.updateProduct = async(req, res, next) => {
+    let product = await Product.findById(req.params.id);
+
+    if(!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found'
+        })
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+}
+
+//Delete Product => /api/v1/admin/product/:id
+
+exports.deleteProduct = async(req, res, next) => {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found'
+        })
+    }
+
+    await product.remove();
+
+    res.status(200).json({
+        success: true,
+        message: 'Product deleted'
+    })
+}
