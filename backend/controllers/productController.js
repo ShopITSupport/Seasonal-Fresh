e1a76f0fcbec11288353efaefd1d83e2db6fcd92
ShopITSupport <83328239+ShopITSupport@users.noreply.github.com>
2021-05-02 18:42:07 +0530

    Search by keyword implementation

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,6 +4,8 @@ const ErrorHandler = require('../utils/errorHandler')
 
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors')
 
+const APIFeatures = require('../utils/apiFeatures');
+
 // Create New Product => api/v1/product/new
 exports.newProduct = catchAsyncErrors(async(req, res, next) => {
     const product = await Product.create(req.body);
@@ -16,7 +18,9 @@ exports.newProduct = catchAsyncErrors(async(req, res, next) => {
 //Get all Products => /api/v1/products
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
 
-    const products = await Product.find();
+    const apiFeatures = new APIFeatures(Product.find(), req.query)
+                        .search()
+    const products = await apiFeatures.query;
 
     res.status(200).json({
         success: true,
