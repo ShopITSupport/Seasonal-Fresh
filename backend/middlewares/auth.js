fdeeba6348f645df721e1d6c706343076c6400b3
ShopITSupport <83328239+ShopITSupport@users.noreply.github.com>
2021-05-03 11:35:06 +0530

    Cookies Implementation

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,20 @@
+const User = require('../models/user')
+const catchAsyncErrors = require('./catchAsyncErrors');
+const ErrorHandler = require('../utils/errorHandler');
+const jwt = require('jsonwebtoken')
+
+// Checks if User is authenticated
+exports.isAuthenticatedUser = catchAsyncErrors(async(req, res, next) => {
+    const { token } = req.cookies
+
+    if (!token) {
+        return next(new ErrorHandler('Login required', 401))
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+    req.user = await User.findById(decoded.id)
+
+    next()
+    
+})
