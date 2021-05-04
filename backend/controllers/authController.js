0e58bc7eaa9f2348b99063070f351a1a31d03e76
ShopITSupport <83328239+ShopITSupport@users.noreply.github.com>
2021-05-04 09:58:17 +0530

    Reset Password

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -8,6 +8,8 @@ const sendToken = require('../utils/jwtToken');
 
 const sendEmail = require('../utils/sendEmail');
 
+const crypto = require('crypto');
+
 // Register User => /api/v1/register
 exports.registerUser = catchAsyncError( async(req, res, next) => {
 
@@ -91,6 +93,34 @@ exports.forgotPassword = catchAsyncError(async(req, res, next) => {
         return next(new ErrorHandler(error.message, 500))
     }
 
+})
+
+//Reset Password => /api/v1/password/reset/:token
+exports.resetPassword = catchAsyncError(async(req, res, next) => {
+    //Hash URL Token
+    const resetPasswordToken = crypto.createHash('sha256').update(req.params.token).digest('hex')
+    
+    const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpire: { $gt: Date.now() }
+    })
+
+    if (!user) {
+        return next(new ErrorHandler('Password reset token is invalid or has been expired', 400))
+    }
+
+    if (req.body.password != req.body.confirmPassword) {
+        return next(new ErrorHandler('Password does not match', 400))
+    }
+
+    //Setup new password
+    user.password = req.body.password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save();
+
+    sendToken(user, 200, res);
 })
 
 //Logout User => /api/v1/logout
