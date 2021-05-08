86add83d54edc4834cd1de77f656398c14850c30
ShopITSupport <83328239+ShopITSupport@users.noreply.github.com>
2021-05-08 18:16:36 +0530

    Filter by Category Completed

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -22,12 +22,23 @@ const Home = ({ match }) => {
 
     const [currentPage, setCurrentPage] = useState(1)
     const [price, setPrice] = useState([1, 1000])
+    const [category, setCategory] = useState('')
+
+    const categories = [
+        'Protiens',
+        'Mutton',
+        'Sea Foods',
+        'Dry Sea Foods',
+        'Vegetables',
+        'Groceries',
+        'Home'
+    ]
 
     const alert = useAlert();
 
     const dispatch = useDispatch();
 
-    const { loading, products, error, productsCount, resPerPage } = useSelector(state => state.products)
+    const { loading, products, error, productsCount, resPerPage, filteredProductsCount } = useSelector(state => state.products)
 
     const keyword = match.params.keyword
 
@@ -36,12 +47,18 @@ const Home = ({ match }) => {
             return alert.error(error);
         }
 
-        dispatch(getProducts(keyword, currentPage, price));
+        dispatch(getProducts(keyword, currentPage, price, category));
         
-    }, [dispatch, alert, error, keyword, currentPage, price]);
+    }, [dispatch, alert, error, keyword, currentPage, price, category]);
 
     function setCurrentPageNo(pageNumber) {
         setCurrentPage(pageNumber)
+    }
+
+    let count = productsCount;
+
+    if (keyword) {
+        count = filteredProductsCount
     }
 
     return (
@@ -73,6 +90,21 @@ const Home = ({ match }) => {
                                                 value={price}
                                                 onChange={price => setPrice(price) }
                                             />
+                                            <hr className="my-5" />
+
+                                            <div className="mt-5">
+                                                <h4 className="mb-3">
+                                                    Categories
+                                                </h4>
+                                                <ul className="pl-0">
+                                                    {categories.map(category => (
+                                                        <li style={{cursor: 'pointer', listStyleType: 'none'}} key={category} onClick={() => setCategory(category)}>
+                                                            {category}
+                                                        </li>
+                                                    ))}
+                                                </ul>
+                                            </div>
+
                                         </div>
                                     </div>
                                     <div className="col-6 col-md-9">
@@ -92,12 +124,12 @@ const Home = ({ match }) => {
                             )}
                         </div>
                     </section>
-                    {resPerPage <= productsCount && (
+                    {resPerPage <= count && (
                         <div className="d-flex justify-content-center mt-5">
                             <Pagination
                                 activePage={currentPage}
                                 itemsCountPerPage={resPerPage}
-                                totalItemsCount={productsCount}
+                                totalItemsCount={count}
                                 onChange={setCurrentPageNo}
                                 nextPageText={'Next'}
                                 prevPageText={'Previous'}
