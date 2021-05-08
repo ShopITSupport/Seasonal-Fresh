cbb921d07acc18adabd56d3123dc26ddadcba6fc
ShopITSupport <83328239+ShopITSupport@users.noreply.github.com>
2021-05-08 14:52:23 +0530

    Pagination Implemented

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,4 +1,6 @@
-import React, { Fragment, useEffect } from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
+import Pagination from 'react-js-pagination'
+
 import MetaData from './layout/MetaData'
 
 import Product from './product/Product'
@@ -11,20 +13,26 @@ import { useAlert } from 'react-alert'
 
 const Home = () => {
 
+    const [currentPage, setCurrentPage] = useState(1)
+
     const alert = useAlert();
 
     const dispatch = useDispatch();
 
-    const { loading, products, error, productsCount } = useSelector(state => state.products)
+    const { loading, products, error, productsCount, resPerPage } = useSelector(state => state.products)
 
     useEffect(() => {
         if (error) {
             return alert.error(error);
         }
 
-        dispatch(getProducts());
+        dispatch(getProducts(currentPage));
         
-    }, [dispatch, alert, error]);
+    }, [dispatch, alert, error, currentPage]);
+
+    function setCurrentPageNo(pageNumber) {
+        setCurrentPage(pageNumber)
+    }
 
     return (
         <Fragment>
@@ -40,6 +48,21 @@ const Home = () => {
                             ))}
                         </div>
                     </section>
+                    {resPerPage <= productsCount && (
+                        <div className="d-flex justify-content-center mt-5">
+                            <Pagination
+                                activePage={currentPage}
+                                itemsCountPerPage={resPerPage}
+                                totalItemsCount={productsCount}
+                                onChange={setCurrentPageNo}
+                                nextPageText={'Next'}
+                                prevPageText={'Previous'}
+                                firstPageText={'First'}
+                                lastPageText={'Last'}
+                                itemClass='page-item'
+                                linkClass='page-link' />
+                        </div>
+                    )}
                 </Fragment>
             )}
             
--- frontend/src/components/product/ProductDetails.js
+++ frontend/src/components/product/ProductDetails.js
@@ -29,8 +29,9 @@ const ProductDetails = ({ match }) => {
 
     return (
         <Fragment>
-        {loading ? <Loader /> : (
+            {loading ? <Loader /> : (
             <Fragment>
+                <MetaData title={product.name}/>
                 <div className="row f-flex justify-content-around">
                     <div className="col-12 col-lg-5 img-fluid" id="product_image">
                         <Carousel pause='hover'>
