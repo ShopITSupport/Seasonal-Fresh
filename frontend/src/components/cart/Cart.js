0bcf831071674975f314337e18d98c6a75869bf8
ShopITSupport <83328239+ShopITSupport@users.noreply.github.com>
2021-05-13 09:34:06 +0530

    Remove Cart Item

--- frontend/src/components/cart/Cart.js
+++ frontend/src/components/cart/Cart.js
@@ -5,47 +5,51 @@ import MetaData from '../layout/MetaData'
 
 import { useAlert } from 'react-alert'
 import { useDispatch, useSelector } from 'react-redux'
-import { addItemToCart } from '../../actions/cartActions'
+import { addItemToCart, removeItemFromCart } from '../../actions/cartActions'
 
-
-const Cart = () => {
+const Cart = ({ history }) => {
 
     const dispatch = useDispatch();
 
-    const { cartItems } = useSelector(state => state.cart )
+    const { cartItems } = useSelector(state => state.cart)
+
+    const removeCartItemHandler = (id) => {
+        dispatch(removeItemFromCart(id))
+    }
 
     const increaseQty = (id, quantity, stock) => {
-        const newQty = quantity + 1
+        const newQty = quantity + 1;
 
-        if (newQty > stock) {
-            return;
-        }
+        if (newQty > stock) return;
 
         dispatch(addItemToCart(id, newQty))
     }
 
     const decreaseQty = (id, quantity) => {
-        const newQty = quantity - 1
 
-        if (newQty <= 0) {
-            return;
-        }
+        const newQty = quantity - 1;
+
+        if (newQty <= 0) return;
+
         dispatch(addItemToCart(id, newQty))
+
     }
 
     return (
         <Fragment>
-            <MetaData title={'Cart'} />
-            { cartItems.length === 0 ? <h2 className="mt-5"> Your cart is empty</h2> : (
+            <MetaData title={'Your Cart'} />
+            {cartItems.length === 0 ? <h2 className="mt-5">Your Cart is Empty</h2> : (
                 <Fragment>
                     <h2 className="mt-5">Your Cart: <b>{cartItems.length} items</b></h2>
 
                     <div className="row d-flex justify-content-between">
                         <div className="col-12 col-lg-8">
-                            { cartItems.map( item => (
+
+                            {cartItems.map(item => (
                                 <Fragment>
                                     <hr />
-                                    <div className="cart-item">
+
+                                    <div className="cart-item" key={item.product}>
                                         <div className="row">
                                             <div className="col-4 col-lg-3">
                                                 <img src={item.image} alt="Laptop" height="90" width="115" />
@@ -57,42 +61,43 @@ const Cart = () => {
 
 
                                             <div className="col-4 col-lg-2 mt-4 mt-lg-0">
-                                                <p id="card_item_price"><span>&#8377;</span>{item.price}</p>
+                                                <p id="card_item_price">${item.price}</p>
                                             </div>
 
                                             <div className="col-4 col-lg-3 mt-4 mt-lg-0">
                                                 <div className="stockCounter d-inline">
-                                                    <span className="btn btn-danger minus" onClick={ () => decreaseQty(item.product, item.quantity)}>-</span>
+                                                    <span className="btn btn-danger minus" onClick={() => decreaseQty(item.product, item.quantity)}>-</span>
+
                                                     <input type="number" className="form-control count d-inline" value={item.quantity} readOnly />
 
-                                                    <span className="btn btn-primary plus" onClick = { () => increaseQty(item.product, item.quantity, item.stock)}>+</span>
+                                                    <span className="btn btn-primary plus" onClick={() => increaseQty(item.product, item.quantity, item.stock)}>+</span>
                                                 </div>
                                             </div>
 
                                             <div className="col-4 col-lg-1 mt-4 mt-lg-0">
-                                                <i id="delete_cart_item" className="fa fa-trash btn btn-danger"></i>
+                                                <i id="delete_cart_item" className="fa fa-trash btn btn-danger" onClick={() => removeCartItemHandler(item.product)} ></i>
                                             </div>
 
                                         </div>
                                     </div>
                                     <hr />
                                 </Fragment>
-                            ) )}
-                            
+                            ))}
+
                         </div>
 
-                            <div className="col-12 col-lg-3 my-4">
-                                <div id="order_summary">
-                                    <h4>Order Summary</h4>
-                                    <hr />
-                                    <p>Subtotal:  <span className="order-summary-values">3 (Units)</span></p>
-                                    <p>Est. total: <span className="order-summary-values"><span>&#8377;</span>765.56</span></p>
+                        <div className="col-12 col-lg-3 my-4">
+                            <div id="order_summary">
+                                <h4>Order Summary</h4>
+                                <hr />
+                                <p>Subtotal:  <span className="order-summary-values">3 (Units)</span></p>
+                                <p>Est. total: <span className="order-summary-values">$765.56</span></p>
 
-                                    <hr />
-                                    <button id="checkout_btn" className="btn btn-primary btn-block">Check out</button>
-                                </div>
+                                <hr />
+                                <button id="checkout_btn" className="btn btn-primary btn-block">Check out</button>
                             </div>
                         </div>
+                    </div>
                 </Fragment>
             )}
         </Fragment>
